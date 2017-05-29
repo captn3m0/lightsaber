9295d66042c61827be51f11e2b92f46fb78217a5
Harman Singh <harman28@gmail.com>
2017-05-29 17:27:21 +0530

    Adds route redirects

--- app.rb
+++ app.rb
@@ -5,7 +5,13 @@ require 'yaml'
 
 def get_url(domain_object, rel_route)
   if domain_object.is_a? Hash
-    return domain_object['root'] + "/" + rel_route
+    if domain_object.key? 'root'
+      domain_object['root'] + '/' + rel_route
+    elsif domain_object.key? rel_route
+      domain_object[rel_route]
+    else
+      domain_object.values.first
+    end
   elsif domain_object.is_a? String
     return domain_object
   end
